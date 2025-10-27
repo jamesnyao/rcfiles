513edef06e36b633d01c10834b1a4f42fbed9ffc
James Yao <jamyao@microsoft.com>
2025-10-27 11:26:15 -0700

    add staging url

--- OneDrive - Microsoft/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive - Microsoft/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -90,12 +90,16 @@ function Set-SisoPath() {
 function Set-RemoteOnly() {
   $env:SISO_LIMITS = "fastlocal=0,startlocal=0"
   $env:SISO_EXPERIMENTS = "no-fallback"
-  #$env:REAPI_CAS_ADDRESS = ""
 }
 
 function Set-DevRE() {
   $env:REAPI_CAS_ADDRESS = "rbecasdev.westus3.cloudapp.azure.com:443"
   $env:REAPI_ADDRESS = "rbedev.westus3.cloudapp.azure.com:443"
+}
+
+function Set-StagingRE() {
+  $env:REAPI_CAS_ADDRESS = "rbecasstaging.westus3.cloudapp.azure.com:443"
+  $env:REAPI_ADDRESS = "rbestaging.westus3.cloudapp.azure.com:443"
 }
 
 function Set-ProdRE() {
